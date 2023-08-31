b37f42c550810cae05d82760d1fe2b8766cf077c
sentuhxd <142236429+sentuhxd@users.noreply.github.com>
2023-08-31 15:18:54 -0400

    put moment js and put currentday  on the page

--- script.js
+++ script.js
@@ -1,10 +1,31 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+$(document).ready(function () {
 
-$("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a")); 
+  var todaysDate = moment().format("dddd, MMM Do YYYY");
+  $("#currentDay").html(todaysDate);
 
-$(function () {
+  function timeBlock() {
+    var currentTimeBlock = moment().hours();
+    $(".time-block").each(function() {
+      var timeBlockHour = parseInt($(this).attr("id"));
+
+    });
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+});
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -23,4 +44,4 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
-});
+
